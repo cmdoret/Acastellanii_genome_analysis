c577d25524b324945f2d130204f14fe981aecae9
cmdoret <cyril.mattheydoret@gmail.com>
2019-06-03 18:29:21 +0200

    fix colors in annotations report histogram

--- scripts/annot_stats.R
+++ scripts/annot_stats.R
@@ -13,7 +13,7 @@ out_plot <- args[2]
 ### LOAD DATA ###
 gff <- read_tsv(in_gff, comment="#", col_names=F)
 colnames(gff) <- c("chrom", "source", "type", "start", "end", "score", "strand", "phase", "attributes")
-gff <- gff %>% filter(type %in% c("mRNA", "CDS", "gene", "exon", "tRNA"))
+gff <- gff %>% filter(type %in% c("mRNA", "CDS", "gene", "exon"))
 
 ### COMPUTE STATS ###
 
@@ -36,8 +36,9 @@ clean_name <- strsplit(basename(in_gff), "\\.")[[1]][1]
 # Label generator to get number of samples
 n_obs <- function(x){return(data.frame(y=0, label=paste0("n=", length(x))))}
 
-feature_len <- ggplot(data=gff, aes(x=type, y=log10(end - start))) + 
-    geom_violin(scale='width') + 
+feature_len <- ggplot(data=gff %>%filter(type %in% c("CDS", "gene")), 
+                      aes(x=type, y=log10(end - start))) +
+    geom_violin(scale='width') +
     stat_summary(fun.data = n_obs, geom = "text") +
     theme_minimal() +
     xlab("") + 
@@ -47,17 +48,25 @@ feature_len <- ggplot(data=gff, aes(x=type, y=log10(end - start))) +
 q25 <- unname(quantile(gff_exons$n_exon, 0.25))
 med <- median(gff_exons$n_exon)
 q75 <- unname(quantile(gff_exons$n_exon, 0.75))
-gff_exons <- gff_exons %>% mutate(n_exon_quant = case_when(
-    n_exon <= q25 ~ paste0("25% quantile: ", q25),
-    n_exon >= q75 ~ paste0("75% quantile: ", q75),
-    TRUE          ~ paste0("Median: ", med)
-))
+q_labs <- c(
+    "q25"=paste0("25% quantile: ", q25), 
+    "q75"=paste0("75% quantile: ", q75), 
+    "med"=paste0("Median: ", med)
+)
+gff_exons <- gff_exons %>% 
+  mutate(n_exon_quant = case_when(
+    n_exon <= q25 ~ q_labs["q25"],
+    n_exon >= q75 ~ q_labs["q75"],
+    TRUE          ~ q_labs["med"]))
+gff_exons <- gff_exons %>% 
+    mutate(n_exon_quant = factor(n_exon_quant, ordered=T, 
+                                 levels=unname(q_labs[c('q25', 'med', 'q75')])))
 
 exon_per_gene <- ggplot(data=gff_exons, aes(x=n_exon, fill=n_exon_quant)) +
     scale_fill_manual(values=c("#6666ee", "#999999", "#ee6666")) +
     geom_histogram(binwidth=1) +
-    geom_vline(xintercept=med) + 
-    theme_minimal() + 
+    geom_vline(xintercept=med) +
+    theme_minimal() +
     ylab("Genes") +
     xlab("Exons per genes")
 
