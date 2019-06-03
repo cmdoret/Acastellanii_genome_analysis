86005c20df884d60ff20261bc5689911ea2966f2
cmdoret <cyril.mattheydoret@gmail.com>
2019-06-03 18:32:24 +0200

    get circos input generation working

--- scripts/gen_circos_files.sh
+++ scripts/gen_circos_files.sh
@@ -3,23 +3,32 @@
 # Generating circos input files.
 # cmdoret, 20190603
 
-CIR_DIR="data/input/misc/circos_conf"j
-REF=$1
-SIGHUNT=$2
-INTERPRO=$3
-CAND=$4
+CIR_DIR="data/input/misc/circos_conf"
+REF="$1"
+SIGHUNT="$2"
+INTERPRO="$3"
+CAND="$4"
+
+# Only include scaffolds larger than 100kb
+len_cutoff=100000
+
 # Karyotype
 awk '/^>/ {if (seqlen){print seqlen}; printf "%s ",$0;seqlen=0;next;}
      {seqlen += length($0)}
      END{print seqlen}' $REF |
-  awk 'print "chr","-",$1,$1,"0",$2' \
+  awk -v minlen=$len_cutoff '$2 >= minlen {print "chr","-",$1,$1,"0",$2,"Neff"}' |
+  tr -d '>' \
   >${CIR_DIR}/karyotype.txt
 
 # sighunt DIAS
 tail -n +2 $SIGHUNT >${CIR_DIR}/sighunt_dias.txt
 # Interpro hits (proportion of bacterial hits)
 tail -n +2 $INTERPRO |
-    awk '{print $2,$3,$4,$9}' \
+    awk '{if ($9 == ""){prop=0} else {prop=$9} print $2,$3,$4,prop}' \
     >${CIR_DIR}/interpro_hits.txt 
-# Candidates
+
+# Candidate genes
+tail -n +2  $CAND |
+  awk '{print $1,$2,$3}' \
+  >${CIR_DIR}/hgt_candidates.txt
 
