e2eeec55f321120ac127bf0f86f19872962ef099
Cyril Matthey-Doret <cyril.matthey-doret@unil.ch>
2019-06-05 15:47:04 +0200

    write basic GO enrichment code using topGO

--- scripts/go_enrich.R
+++ scripts/go_enrich.R
@@ -7,26 +7,29 @@ library(topGO)
 # Parse CL args
 args <- commandArgs(trailingOnly=T)
 
-annot_tbl <- read_tsv(args[1])
-candidates <- read_tsv(args[2])
-out <- args[3]
+# Extract gene IDs and GO terms.
+annot_tbl <- read_tsv(args[1], col_types='ccciiccccccccccccccc')
+# Write geneID - GO terms mapping to file
+write_tsv(annot_tbl, tmp_dir)
+hgt_candidates <- read_tsv(args[2]) %>%
+  pull(GeneID)
+out_file <- args[4]
+tmp_mapfile <- paste(dirname(out_file), 'id2go.tsv', sep='/')
 
-# Input format is:
-# 133103  GO:0015031, GO:0005794, GO:0016020, GO:0017119, GO:0000139
-# 121005  GO:0005576
-# 155158  GO:0005488
-# 160828  GO:0005488
-# 105778  GO:0016021, GO:0016020
-# 166881  GO:0003674, GO:0016021, GO:0016020, GO:0008150
+annot_tbl %>%
+  dplyr::select(GeneID, `GO Terms`) %>%
+  mutate(`GO Terms` = str_replace_all(`GO Terms`, ";", ", ")) %>%
+  write_tsv(tmp_mapfile)
 
-geneID2GO <- readMappings(map_file)
 
-sampleGOdata <- new("topGOdata",
-                    description = "HGT analyis in A. castellanii",
-                    ontology = "BP",
-                    allGenes = geneList, 
-                    geneSel = topDiffGenes,
-                    nodeSize = 10,
-                    annot = annFUN.db,
-                    affyLib = affyLib)
+geneID2GO <- readMappings(tmp_mapfile)
 
+GOdata <- new("topGOdata",
+              description = "HGT analyis in A. castellanii",
+              ontology = "BP",
+              allGenes = annot_tbl %>% pull(GeneID), 
+              geneSel = hgt_candidates,
+              nodeSize = 10,
+              gene2GO = geneID2GO)
+
+showSigOfNodes(sampleGOdata, score(resultKS.elim), firstSigNodes = 5, useInfo = 'all')
